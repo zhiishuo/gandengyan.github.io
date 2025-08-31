b43e49bb247f60d24e2876670d8cffa55dbea293
zhiishuo <82660676+zhiishuo@users.noreply.github.com>
2025-08-31 09:18:07 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'gandengyan-diamante-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )));
+});
+
+self.addEventListener('fetch', e=>{
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req))
+  );
+});
